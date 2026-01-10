3324d088f31e69fac6256f81934e8f41d91ee030
Gabriel Oliveira <gabriel9938@gmail.com>
2026-01-10 11:28:12 -0300

    refactor: simplify data update logic by removing unused formatting function

--- src/app/api/update/route.ts
+++ src/app/api/update/route.ts
@@ -1,5 +1,4 @@
 import { IData, IUpdateData } from "@/types/data";
-import { isBefore, isEqual } from "date-fns";
 import { revalidateTag } from "next/cache";
 
 export async function PUT(request: Request) {
@@ -18,9 +17,7 @@ export async function PUT(request: Request) {
 
     const { data: dataList }: { data: IData[] } = await dataListRes.json();
 
-    const formattedData = formatData(dataList, jsonBody);
-
-    const updatedData = JSON.stringify(formattedData);
+    const updatedData = JSON.stringify(dataList);
 
     const response = await fetch(
       `${process.env.DATABASE_URL}/list.json?auth=${token}`,
@@ -44,49 +41,3 @@ export async function PUT(request: Request) {
     return;
   }
 }
-
-const formatData = (data: IData[], updateData: IUpdateData) => {
-  return data.map((item) => {
-    const splinedDate = item["Data"].split("/");
-    const splinedSelectedDate = updateData.date.split("/");
-
-    const date = new Date(
-      `${splinedDate[1]}-${splinedDate[0]}-${splinedDate[2]}`
-    );
-    const selectedDate = new Date(
-      `${splinedSelectedDate[1]}-${splinedSelectedDate[0]}-${splinedSelectedDate[2]}`
-    );
-
-    if (
-      updateData.date === "Todos" &&
-      item["Produto"].trim() === updateData.product.trim()
-    ) {
-      return {
-        ...item,
-        ["Quantidade"]: item["Quantidade"] * updateData.divided,
-        ["Preço unitário"]: item["Preço unitário"] / updateData.divided,
-        ["Valor da Operação"]:
-          item["Quantidade"] *
-          updateData.divided *
-          (item["Preço unitário"] / updateData.divided),
-      };
-    }
-
-    if (
-      (isEqual(date, selectedDate) || isBefore(date, selectedDate)) &&
-      item["Produto"].trim() === updateData.product.trim()
-    ) {
-      return {
-        ...item,
-        ["Quantidade"]: item["Quantidade"] * updateData.divided,
-        ["Preço unitário"]: item["Preço unitário"] / updateData.divided,
-        ["Valor da Operação"]:
-          item["Quantidade"] *
-          updateData.divided *
-          (item["Preço unitário"] / updateData.divided),
-      };
-    }
-
-    return item;
-  });
-};
